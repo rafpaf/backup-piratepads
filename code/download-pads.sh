eb5f2091e53629308da07dc71351e4ef891aeda2
Raphael Krut-Landau <raphael.kl@gmail.com>
2011-02-18 00:53:33 -0500

    make sure you git commit in the right place

--- code/download-pads.sh
+++ code/download-pads.sh
@@ -2,5 +2,6 @@
 for url in `cat pad_urls`; do
     wget "http://piratepad.net/ep/pad/export/$url/latest?format=html" -O "$url.html"
 done
+cd `dirname $0`
 git add *.html
 git commit -m "automatic commit by code/download-pads.sh"
