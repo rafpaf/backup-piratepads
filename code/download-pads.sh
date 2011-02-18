280ee05d84071db3ebcb39de4e31bccc702aff60
Raphael Krut-Landau <raphael.kl@gmail.com>
2011-02-18 00:54:24 -0500

    make sure to add the pads

--- code/download-pads.sh
+++ code/download-pads.sh
@@ -3,5 +3,5 @@ for url in `cat pad_urls`; do
     wget "http://piratepad.net/ep/pad/export/$url/latest?format=html" -O "$url.html"
 done
 cd `dirname $0`
-git add *.html
+git add ../pads/*.html
 git commit -m "automatic commit by code/download-pads.sh"
